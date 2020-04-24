0ec869b9e1640ee29136a29a45be563a4def07e8
idith almog <almog.idith@gmail.com>
2020-04-24 17:10:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,16 +1,31 @@
 def starts_with_a_vowel?(word)
-
+  if word.match( /\b(?=\w)[aeiou]/ ) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  array=text.split
+  array1=array.grep(/^un/)
+  array2=array1.grep(/ing$/)
+
 
 end
-
 def words_five_letters_long(text)
+   array=text.split
+   array1=array.grep(/^\w{5}\z/)
+
 
 end
-
 def first_word_capitalized_and_ends_with_punctuation?(text)
+
+   if text.match(/^[A-Z]/) != nil && text.match(/[.]$/) != nil
+     true
+   else
+     false
+   end
 
 end
 
